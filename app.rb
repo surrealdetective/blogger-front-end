d032effbc50ea8ffab890e3ad14fdfbb1f420fe8
Blake Johnson <blake41@@gmail.com>
2013-07-03 12:20:28 -0400

    got tux working and students seeded in database

--- app.rb
+++ app.rb
@@ -1,4 +1,18 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'bundler'
+
+Bundler.require
+
+Dir.glob('./lib/*.rb') do |model|
+  require model
+end
 
 set :database, "sqlite3:///student.db"
+
+module Blog 
+
+  class App < Sinatra::Base
+  end
+  
+end
